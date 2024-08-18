af8b8afc1efc92436f5cbbb7bf14cd8e17b9d833
Muhammad Abubakar Jamiu <muhammadabubakarjamiu@gmail.com>
2024-08-18 15:52:33 -0700

    create edit route

--- api/index.js
+++ api/index.js
@@ -201,7 +201,37 @@ app.get('/user-posts', authenticateUser, async (req, res) => {
     res.status(500).json({ message: 'Error fetching posts' });
   }
 });
+// edit post route
+app.put('/post',uploadMiddleware.single('file'), async (req,res) => {
+  let newPath = null;
+  if (req.file) {
+    const {originalname,path} = req.file;
+    const parts = originalname.split('.');
+    const ext = parts[parts.length - 1];
+    newPath = path+'.'+ext;
+    fs.renameSync(path, newPath);
+  }
 
+  const {token} = req.cookies;
+  jwt.verify(token, secret, {}, async (err,info) => {
+    if (err) throw err;
+    const {id,title,summary,content} = req.body;
+    const postDoc = await Post.findById(id);
+    const isAuthor = JSON.stringify(postDoc.author) === JSON.stringify(info.id);
+    if (!isAuthor) {
+      return res.status(400).json('you are not the author');
+    }
+    await postDoc.update({
+      title,
+      summary,
+      content,
+      cover: newPath ? newPath : postDoc.cover,
+    });
+
+    res.json(postDoc);
+  });
+
+});
 // single post route
 
 app.get('/post/:id', async (req, res) => {
